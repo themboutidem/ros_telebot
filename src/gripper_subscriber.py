1b77cb21717bcf59f866d88112e54c709209459a
Mboutidem Mkpong <mboutidemmkpong1@gmail.com>
2024-02-23 23:52:06 -0500

    Refactor gripper_control & gripper_subscriber into single file
    Combines all Subscriber commands are in
    gripper_subscriber.

--- src/gripper_subscriber.py
+++ src/gripper_subscriber.py
@@ -1,22 +1,105 @@
 #!/usr/bin/env python
-
+import sys
 import rospy
+import time
+
+from kortex_driver.srv import *
+from kortex_driver.msg import *
+
 from std_msgs.msg import Float32
-from gripper_control import GripperControl
 
-def gripper_callback(values):
-    rospy.loginfo("Received gripper signal: %f", values.data)
-    gripper_control.gripper_action(values)
+class GripperSubscriber:
+    def __init__(self):
+        try:
+            self.HOME_ACTION_IDENTIFIER = 2
 
-if __name__ == "__main__":
-    rospy.init_node('gripper_subscriber')
+            # Get node params
+            self.robot_name = "telebot_right_arm"
+            self.degrees_of_freedom = rospy.get_param("/" + self.robot_name + "/degrees_of_freedom", 7)
+            self.is_gripper_present = rospy.get_param("/" + self.robot_name + "/is_gripper_present", True)
 
-    gripper_control = GripperControl()
+            rospy.loginfo("Using robot_name " + self.robot_name + " , robot has " + str(self.degrees_of_freedom) + " degrees of freedom and is_gripper_present is " + str(self.is_gripper_present))
 
-    if gripper_control.is_init_success:
-        rospy.Subscriber("/gripper_signal", Float32, gripper_callback)
+            # Init the action topic subscriber
+            self.action_topic_sub = rospy.Subscriber("/" + self.robot_name + "/action_topic", ActionNotification, self.cb_action_topic)
+            self.last_action_notif_type = None
 
+            # Init the services
+            clear_faults_full_name = '/' + self.robot_name + '/base/clear_faults'
+            rospy.wait_for_service(clear_faults_full_name)
+            self.clear_faults = rospy.ServiceProxy(clear_faults_full_name, Base_ClearFaults)
+
+            read_action_full_name = '/' + self.robot_name + '/base/read_action'
+            rospy.wait_for_service(read_action_full_name)
+            self.read_action = rospy.ServiceProxy(read_action_full_name, ReadAction)
+
+            execute_action_full_name = '/' + self.robot_name + '/base/execute_action'
+            rospy.wait_for_service(execute_action_full_name)
+            self.execute_action = rospy.ServiceProxy(execute_action_full_name, ExecuteAction)
+
+            set_cartesian_reference_frame_full_name = '/' + self.robot_name + '/control_config/set_cartesian_reference_frame'
+            rospy.wait_for_service(set_cartesian_reference_frame_full_name)
+            self.set_cartesian_reference_frame = rospy.ServiceProxy(set_cartesian_reference_frame_full_name, SetCartesianReferenceFrame)
+
+            send_gripper_command_full_name = '/' + self.robot_name + '/base/send_gripper_command'
+            rospy.wait_for_service(send_gripper_command_full_name)
+            self.send_gripper_command = rospy.ServiceProxy(send_gripper_command_full_name, SendGripperCommand)
+
+            activate_publishing_of_action_notification_full_name = '/' + self.robot_name + '/base/activate_publishing_of_action_topic'
+            rospy.wait_for_service(activate_publishing_of_action_notification_full_name)
+            self.activate_publishing_of_action_notification = rospy.ServiceProxy(activate_publishing_of_action_notification_full_name, OnNotificationActionTopic)
+            
+            get_product_configuration_full_name = '/' + self.robot_name + '/base/get_product_configuration'
+            rospy.wait_for_service(get_product_configuration_full_name)
+            self.get_product_configuration = rospy.ServiceProxy(get_product_configuration_full_name, GetProductConfiguration)
+
+            validate_waypoint_list_full_name = '/' + self.robot_name + '/base/validate_waypoint_list'
+            rospy.wait_for_service(validate_waypoint_list_full_name)
+            self.validate_waypoint_list = rospy.ServiceProxy(validate_waypoint_list_full_name, ValidateWaypointList)
+        
+        except:
+            self.is_init_success = False
+        else:
+            rospy.loginfo("GripperSubscriber initialized successfully")
+            self.is_init_success = True
+
+    def cb_action_topic(self,notif):
+        self.last_action_notif_type = notif.action_event
+
+    def gripper_callback(self, values):
+        rospy.loginfo("Received gripper signal: %f", values.data)
+
+        #Establish Gripper Connection
+        value = values.data
+        req = SendGripperCommandRequest()
+        finger = Finger()
+        finger.finger_identifier = 0
+        finger.value = value
+        req.input.gripper.finger.append(finger)
+        req.input.mode = GripperMode.GRIPPER_POSITION
+
+        rospy.loginfo("Sending the gripper command...")
+
+        # Call the service 
+        try:
+            self.send_gripper_command(req)
+        except rospy.ServiceException:
+            rospy.logerr("Failed to call SendGripperCommand")
+            return False
+        else:
+            #time.sleep(0.1)
+            return True
+
+    def main(self):
+        #Create Subscriber Node
+        rospy.init_node('gripper_subscriber')
+
+        #Listen & Send Gripper Signal to Callback Function
+        rospy.Subscriber("/gripper_signal", Float32, self.gripper_callback)
         rospy.loginfo("Gripper subscriber node is running.")
         rospy.spin()
-    else:
-        rospy.logerr("Failed to initialize GripperControl.")
+
+if __name__ == "__main__":
+    run = GripperSubscriber()
+    run.main()
+
