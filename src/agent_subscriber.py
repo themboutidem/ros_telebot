1d3b43beb869fb650c50803da5d8bdc617fe6cb6
Mboutidem Mkpong <mboutidemmkpong1@gmail.com>
2024-02-28 21:29:36 -0500

    Create Subscriber for Custom IMU Data
    Made correction to package name in CMakeLists.txt
    Created subscriber file and wrote code to listen for
    custom IMU Data on topic: /imu_coordinates

---
+++ src/agent_subscriber.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+import sys
+import rospy
+import time
+
+from kortex_driver.srv import *
+from kortex_driver.msg import *
+
+from custom_msgs.msg import IMUData
+
+class AgentSubscriber:
+    def __init__(self):
+        try:
+            self.HOME_ACTION_IDENTIFIER = 2
+
+            # Get node params
+            self.robot_name = "telebot_right_arm"
+            self.degrees_of_freedom = rospy.get_param("/" + self.robot_name + "/degrees_of_freedom", 7)
+            self.is_gripper_present = rospy.get_param("/" + self.robot_name + "/is_gripper_present", True)
+
+            rospy.loginfo("Using robot_name " + self.robot_name + " , robot has " + str(self.degrees_of_freedom) + " degrees of freedom and is_gripper_present is " + str(self.is_gripper_present))
+
+            # Init the action topic subscriber
+            self.action_topic_sub = rospy.Subscriber("/" + self.robot_name + "/action_topic", ActionNotification, self.cb_action_topic)
+            self.last_action_notif_type = None
+
+            # Init the services
+            clear_faults_full_name = '/' + self.robot_name + '/base/clear_faults'
+            rospy.wait_for_service(clear_faults_full_name)
+            self.clear_faults = rospy.ServiceProxy(clear_faults_full_name, Base_ClearFaults)
+
+            read_action_full_name = '/' + self.robot_name + '/base/read_action'
+            rospy.wait_for_service(read_action_full_name)
+            self.read_action = rospy.ServiceProxy(read_action_full_name, ReadAction)
+
+            execute_action_full_name = '/' + self.robot_name + '/base/execute_action'
+            rospy.wait_for_service(execute_action_full_name)
+            self.execute_action = rospy.ServiceProxy(execute_action_full_name, ExecuteAction)
+
+            set_cartesian_reference_frame_full_name = '/' + self.robot_name + '/control_config/set_cartesian_reference_frame'
+            rospy.wait_for_service(set_cartesian_reference_frame_full_name)
+            self.set_cartesian_reference_frame = rospy.ServiceProxy(set_cartesian_reference_frame_full_name, SetCartesianReferenceFrame)
+
+            send_gripper_command_full_name = '/' + self.robot_name + '/base/send_gripper_command'
+            rospy.wait_for_service(send_gripper_command_full_name)
+            self.send_gripper_command = rospy.ServiceProxy(send_gripper_command_full_name, SendGripperCommand)
+
+            activate_publishing_of_action_notification_full_name = '/' + self.robot_name + '/base/activate_publishing_of_action_topic'
+            rospy.wait_for_service(activate_publishing_of_action_notification_full_name)
+            self.activate_publishing_of_action_notification = rospy.ServiceProxy(activate_publishing_of_action_notification_full_name, OnNotificationActionTopic)
+            
+            get_product_configuration_full_name = '/' + self.robot_name + '/base/get_product_configuration'
+            rospy.wait_for_service(get_product_configuration_full_name)
+            self.get_product_configuration = rospy.ServiceProxy(get_product_configuration_full_name, GetProductConfiguration)
+
+            validate_waypoint_list_full_name = '/' + self.robot_name + '/base/validate_waypoint_list'
+            rospy.wait_for_service(validate_waypoint_list_full_name)
+            self.validate_waypoint_list = rospy.ServiceProxy(validate_waypoint_list_full_name, ValidateWaypointList)
+        
+        except:
+            self.is_init_success = False
+        else:
+            rospy.loginfo("GripperSubscriber initialized successfully")
+            self.is_init_success = True
+
+    def cb_action_topic(self,notif):
+        self.last_action_notif_type = notif.action_event
+
+    def FillCartesianWaypoint(self, new_x, new_y, new_z, new_theta_x, new_theta_y, new_theta_z, blending_radius):
+        waypoint = Waypoint()
+        cartesianWaypoint = CartesianWaypoint()
+
+        cartesianWaypoint.pose.x = new_x
+        cartesianWaypoint.pose.y = new_y
+        cartesianWaypoint.pose.z = new_z
+        cartesianWaypoint.pose.theta_x = new_theta_x
+        cartesianWaypoint.pose.theta_y = new_theta_y
+        cartesianWaypoint.pose.theta_z = new_theta_z
+        cartesianWaypoint.reference_frame = CartesianReferenceFrame.CARTESIAN_REFERENCE_FRAME_BASE
+        cartesianWaypoint.blending_radius = blending_radius
+        waypoint.oneof_type_of_waypoint.cartesian_waypoint.append(cartesianWaypoint)
+
+        return waypoint
+
+    def wait_for_action_end_or_abort(self):
+        while not rospy.is_shutdown():
+            if (self.last_action_notif_type == ActionEvent.ACTION_END):
+                rospy.loginfo("Received ACTION_END notification")
+                return True
+            elif (self.last_action_notif_type == ActionEvent.ACTION_ABORT):
+                rospy.loginfo("Received ACTION_ABORT notification")
+                return False
+            else:
+                time.sleep(0.01)
+
+    def cartesian_waypoint_action(self, values):
+        rospy.loginfo("Received agent IMU signal.")
+        self.last_action_notif_type = None
+
+        req = ExecuteActionRequest()
+        trajectory = WaypointList()
+
+        config = self.get_product_configuration()
+
+        rospy.loginfo("Sending the waypoint command...")
+
+        #if config.output.model == ModelId.MODEL_ID_L31:
+        trajectory.waypoints.append(self.FillCartesianWaypoint(values.x,  values.y,  values.z, values.roll, values.pitch, values.yaw, 0))
+            
+        req.input.oneof_action_parameters.execute_waypoint_list.append(trajectory)
+        
+        # Call the service
+        rospy.loginfo("Executing Kortex action ExecuteWaypointTrajectory...")
+        try:
+            self.execute_action(req)
+        except rospy.ServiceException:
+            rospy.logerr("Failed to call action ExecuteWaypointTrajectory")
+            return False
+        else:
+            return self.wait_for_action_end_or_abort()
+
+    def main(self):
+        #Create Subscriber Node
+        rospy.init_node('imu_subscriber')
+
+        #Listen & Send Gripper Signal to Callback Function
+        rospy.Subscriber("/imu_coordinates", IMUData, self.cartesian_waypoint_action)
+        rospy.loginfo("Agent subscriber node is running.")
+        rospy.spin()
+
+if __name__ == "__main__":
+    run = AgentSubscriber()
+    run.main()
+
